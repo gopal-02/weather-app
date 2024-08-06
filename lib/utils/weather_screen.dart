cb7899328227303d82283da31aa06f804e435724
Gopal Kumar <gopalkumar8220@gmail.com>
2024-08-06 23:27:14 +0530

    Add forcast cards

--- lib/utils/weather_screen.dart
+++ lib/utils/weather_screen.dart
@@ -1,12 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/utils/hourly_forecast_cards.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar:AppBar(title: const Text("Weather App"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print("Refresh");
+            },
+            icon: const Icon(
+              Icons.refresh,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                child: Column(
+                  children: [
+                    Text('20 °C',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    Icon(
+                      Icons.wb_sunny,
+                      size: 64,
+                      color: Colors.orange,
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      'Sunday',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Hourly Forecast',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForecastCard(),
+                  HourlyForecastCard(),
+                  HourlyForecastCard(),
+                  HourlyForecastCard(),
+                  HourlyForecastCard(),
+                  HourlyForecastCard()
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Additional Info',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
